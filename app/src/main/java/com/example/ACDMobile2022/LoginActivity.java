b8038289e5993c89de13f0277634dbe8ad0941c1
BiasiCamillo <camillo.biasi96@gmail.com>
2022-03-30 12:45:35 +0200

    fixs

--- app/src/main/java/com/example/ACDMobile2022/LoginActivity.java
+++ app/src/main/java/com/example/ACDMobile2022/LoginActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import com.example.ACDMobile2022.Fragment.LoginFragment;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,20 +18,32 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        addFragmentLogin();
+//       TextView dontHaveAccount = (TextView) findViewById(R.id.dontHaveAccount);
+//
+//       dontHaveAccount.setOnClickListener(new View.OnClickListener() {
+//           @Override
+//           public void onClick(View view) {
+//               openRegisterActivity();
+//           }
+//       });
 
-       TextView dontHaveAccount = (TextView) findViewById(R.id.dontHaveAccount);
+    }
 
-       dontHaveAccount.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               openRegisterActivity();
-           }
-       });
-
+    @Override
+    protected void onResume() {
+        super.onResume();
+        addFragmentLogin();
     }
 
     public void openRegisterActivity (){
         Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
         startActivity(intent);
+    }
+
+    public void addFragmentLogin(){
+        LoginFragment loginFragment = new LoginFragment();
+        Log.d("passo 1","sono all");
+        getSupportFragmentManager().beginTransaction().add(R.id.frame_loginFragment,LoginFragment.class, null).commit();
     }
 }
--- app/src/main/java/com/example/ACDMobile2022/MainActivity.java
+++ app/src/main/java/com/example/ACDMobile2022/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.ACDMobile2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -10,12 +12,13 @@ public class MainActivity extends AppCompatActivity {
 
     private Button button;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        Intent goLogin = new Intent(this, LoginActivity.class);
+        startActivity(goLogin);
 
     }
 
